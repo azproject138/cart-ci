142e99c54e74a6e904b0eb3fbcfe2d94d0cfe46a
indrodev <pstar3591@gmail.com>
2024-11-20 13:29:11 +0700

    menambahkan code di file Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/register', 'Auth::register');
+$routes->post('/register', 'Auth::processRegister');
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::processLogin');
+$routes->get('/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Dashboard::index');
+
