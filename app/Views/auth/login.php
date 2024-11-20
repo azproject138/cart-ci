6ac9c4017a466920481e84b8f81e8e999e7a4c2c
indrodev <pstar3591@gmail.com>
2024-11-20 13:25:25 +0700

    menambahkan code difile login.php

---
+++ app/Views/auth/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <?php if (session()->getFlashdata('error')): ?>
+        <p style="color: red;"><?= session()->getFlashdata('error') ?></p>
+    <?php endif; ?>
+    <form action="/login" method="post">
+        <?= csrf_field() ?>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit">Login</button>
+    </form>
+    <a href="/register">Don't have an account? Register here</a>
+</body>
+</html>
