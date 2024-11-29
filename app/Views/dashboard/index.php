9c21819a7a8bdc32a7f318988ae492a1bc5dd4d6
indrodev <pstar3591@gmail.com>
2024-11-29 20:24:39 +0700

    menambahkan navbar difile index.php dan menambahkan contoh file JPG untuk contoh logo profile

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -8,10 +8,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Tambahkan Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
-    <h2>Welcome, <?= $username ?>!</h2>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Welcome, <?= $username ?>!
+                </a>
+            </div>
+        </nav>
+    </header>
     <a href="/logout">Logout</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
