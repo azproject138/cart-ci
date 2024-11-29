18a0f3cf29236b426d816d3f6479d0cc2add8fb1
indrodev <pstar3591@gmail.com>
2024-11-29 19:53:16 +0700

    menambhakan code <meta> dan <link> bootstrap difile index.php

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <!-- Tambahkan Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Tambahkan Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
     <h2>Welcome, <?= $username ?>!</h2>
