c7b40d99ccbb03154e84fbb822cc809c3ae4efa0
indrodev <pstar3591@gmail.com>
2024-12-01 21:00:07 +0700

    mengubah class pada file index.php

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Tambahkan Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link rel="stylesheet" href="/assets/style.css">
 </head>
@@ -32,11 +33,15 @@
                 <li><a href="#">About</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="/logout">Logout</a></li>
             </ul>
         </div>
+        <div id="main-content">
+            <button id="open-btn" class="open-btn">☰</button>
+        </div>
     </nav>
-    <a href="/logout">Logout</a>
 
+    <script src="/assets/sidebar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
