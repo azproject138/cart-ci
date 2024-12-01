b599444fbe6741862e36723032ff5d4f2b5b1953
indrodev <pstar3591@gmail.com>
2024-12-01 20:48:49 +0700

    menambahkan sidebar difile index.php, halaman dashboard

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -12,16 +12,30 @@
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
+    <!--navbar-->
     <header>
-        <nav class="navbar bg-body-tertiary">
+        <div class="navbar bg-body-tertiary">
             <div class="container-fluid">
                 Welcome, <?= $username ?>
                 <a class="navbar-brand" href="#">
                     <img src="/assets/img/logo-profile.png" alt="Logo" width="25" height="25" class="d-inline-block align-text-top">
                 </a>
             </div>
-        </nav>
+        </div>
     </header>
+    <nav>
+        <!--sidebar-->
+        <div id="sidebar" class="sidebar">
+            <button id="close-btn" class="close-btn">×</button>
+            <ul>
+                <h3 class="username">Welcome!</h3>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
     <a href="/logout">Logout</a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
