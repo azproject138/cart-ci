0e9fa63653a8e30ee67501fb3fcd94583bd16af3
indrodev <pstar3591@gmail.com>
2024-12-03 19:30:05 +0700

    menambahkan code database difile CreateUserProfile

--- app/Database/Migrations/2024-12-03-122452_CreateUsersProfile.php
+++ app/Database/Migrations/2024-12-03-122452_CreateUsersProfile.php
@@ -8,11 +8,42 @@ class CreateUsersProfile extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'profile_picture' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'address' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'whatsapp' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
