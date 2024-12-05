52924c59fda87f6381e6165d676f915f04a55c6b
indrodev <pstar3591@gmail.com>
2024-12-05 13:25:22 +0700

    mengembalikan code script toogglePassword.js

--- public/assets/togglePassword.js
+++ public/assets/togglePassword.js
@@ -10,13 +10,4 @@ function togglePassword(fieldId, iconId) {
         icon.classList.remove("bi-eye-slash");
         icon.classList.add("bi-eye");
     }
-    if (field.type === "password_confirmation") {
-        field.type = "text";
-        icon.classList.remove("bi-eye");
-        icon.classList.add("bi-eye-slash");
-    } else {
-        field.type = "password_confirmation";
-        icon.classList.remove("bi-eye-slash");
-        icon.classList.add("bi-eye");
-    }
 }
