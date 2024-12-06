3943033bf928084503398804b8ef12493ec4a34c
indrodev <pstar3591@gmail.com>
2024-12-06 17:16:53 +0700

    menghapus komentar '//' pada file toogglePassword.js

--- public/assets/togglePassword.js
+++ public/assets/togglePassword.js
@@ -1,16 +1,16 @@
-//function togglePassword(fieldId, iconId) {
-//    const field = document.getElementById(fieldId);
-//    const icon = document.getElementById(iconId);
-//    if (field.type === "password") {
-//        field.type = "text";
-//        icon.classList.remove("bi-eye");
-//        icon.classList.add("bi-eye-slash");
-//    } else {
-//        field.type = "password";
-//        icon.classList.remove("bi-eye-slash");
-//        icon.classList.add("bi-eye");
-//    }
-//}
+function togglePassword(fieldId, iconId) {
+    const field = document.getElementById(fieldId);
+    const icon = document.getElementById(iconId);
+    if (field.type === "password") {
+        field.type = "text";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    } else {
+        field.type = "password";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+    }
+}
 //confirmasi password
 function togglePassword(fieldId) {
     const field = document.getElementById(fieldId);
