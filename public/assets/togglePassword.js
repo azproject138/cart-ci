7cbe1f2e466026f594f937e44e378ec455a8070e
indrodev <pstar3591@gmail.com>
2024-12-06 17:18:27 +0700

    menghapus script pada baris ke 14-27 pada file toogglePassword.js

--- public/assets/togglePassword.js
+++ public/assets/togglePassword.js
@@ -11,17 +11,3 @@ function togglePassword(fieldId, iconId) {
         icon.classList.add("bi-eye");
     }
 }
-//confirmasi password
-function togglePassword(fieldId) {
-    const field = document.getElementById(fieldId);
-    const icon = document.getElementById(`${fieldId}-icon`);
-    if (field.type === "password") {
-        field.type = "text";
-        icon.classList.remove("bi-eye-slash");
-        icon.classList.add("bi-eye");
-    } else {
-        field.type = "password";
-        icon.classList.remove("bi-eye");
-        icon.classList.add("bi-eye-slash");
-    }
-}
