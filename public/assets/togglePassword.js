24f2bae903f29d09f725d6ee318e145adfe275f5
indrodev <pstar3591@gmail.com>
2024-12-06 17:21:04 +0700

    mengembalikan code script pada baris ke 14-27 dan menghapus script pada baris ke 1-14 di file toogglePassword.js

--- public/assets/togglePassword.js
+++ public/assets/togglePassword.js
@@ -1,13 +1,13 @@
-function togglePassword(fieldId, iconId) {
+function togglePassword(fieldId) {
     const field = document.getElementById(fieldId);
-    const icon = document.getElementById(iconId);
+    const icon = document.getElementById(`${fieldId}-icon`);
     if (field.type === "password") {
         field.type = "text";
-        icon.classList.remove("bi-eye");
-        icon.classList.add("bi-eye-slash");
-    } else {
-        field.type = "password";
         icon.classList.remove("bi-eye-slash");
         icon.classList.add("bi-eye");
+    } else {
+        field.type = "password";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
     }
 }
