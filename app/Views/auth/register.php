680cf6686266f13e23881fa7231ff262e12daede
indrodev <pstar3591@gmail.com>
2024-12-07 09:36:42 +0700

    menambahkan css pada file register.php di baris 49 dan 51, dan 56 dan 58

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -46,16 +46,16 @@
                     </div>
                     <div class="mb-3 position-relative">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
+                        <input type="password" name="password" id="password" class="form-control" style="padding: 14px; border-radius: 0; border: none; background-color: #f2f2f2;" required>
                         <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" onclick="togglePassword('password')">
-                            <i class="bi bi-eye-slash" id="password-icon"></i>
+                            <i class="bi bi-eye-slash" style=" margin-right: 10px;" id="password-icon"></i>
                         </span>
                     </div>
                     <div class="mb-3 position-relative">
                         <label for="confirm_password" class="form-label">Confirm Password</label>
-                        <input type="password" name="confirm_password" id="confirm_password" class="form-control" required>
+                        <input type="password" name="confirm_password" id="confirm_password" class="form-control" style="padding: 14px; border-radius: 0; border: none; background-color: #f2f2f2;" required>
                         <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" onclick="togglePassword('confirm_password')">
-                            <i class="bi bi-eye-slash" id="confirm_password-icon"></i>
+                            <i class="bi bi-eye-slash" style=" margin-right: 10px;" id="confirm_password-icon"></i>
                         </span>
                     </div>
                     <button class="submit-btn-register" type="submit">Register</button>
