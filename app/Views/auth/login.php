806306142b33c61e5c70e14fd86165d9464d48e1
indrodev <pstar3591@gmail.com>
2024-12-07 09:34:50 +0700

    menambahkan css pada file logi.php dan dibaris 43 dan 45

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -40,9 +40,9 @@
 
                     <div class="mb-3 position-relative">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" style="padding: 14px;" required>
-                        <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer;" onclick="togglePassword('password')">
-                            <i class="bi bi-eye-slash" id="password-icon"></i>
+                        <input type="password" name="password" id="password" class="form-control" style="padding: 14px; border-radius: 0; border: none; background-color: #f2f2f2;" placeholder="Password" required>
+                        <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer; margin-top: 10px;" onclick="togglePassword('password')">
+                            <i class="bi bi-eye-slash" style=" margin-right: 10px;" id="password-icon"></i>
                         </span>
                     </div>
 
