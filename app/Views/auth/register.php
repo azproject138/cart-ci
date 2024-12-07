333940d5e395c5b491bda63511b060ac941f97f6
indrodev <pstar3591@gmail.com>
2024-12-07 09:51:32 +0700

    menambahkan class form-input-pass pada file register.php di baris 47 da 54, menambahkan css pada class form-input-pass

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -44,14 +44,14 @@
                         <label for="email" class="form-label">Email :</label>
                         <input type="email" name="email" placeholder="Email" value="<?= old('email') ?>" required>
                     </div>
-                    <div class="position-relative">
+                    <div class="form-input-pass position-relative">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" name="password" placeholder="Password" id="password" class="form-control" style="padding: 14px; border-radius: 0; border: none; background-color: #f2f2f2;" required>
                         <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer; margin-top: 10px;" onclick="togglePassword('password')">
                             <i class="bi bi-eye-slash" style=" margin-right: 10px;" id="password-icon"></i>
                         </span>
                     </div>
-                    <div class="position-relative">
+                    <div class="form-input-pass position-relative">
                         <label for="confirm_password" class="form-label">Confirm Password</label>
                         <input type="password" name="confirm_password" placeholder="Confirmasi Password" id="confirm_password" class="form-control" style="padding: 14px; border-radius: 0; border: none; background-color: #f2f2f2;" required>
                         <span class="position-absolute top-50 end-0 translate-middle-y me-2" style="cursor: pointer; margin-top: 10px;" onclick="togglePassword('confirm_password')">
