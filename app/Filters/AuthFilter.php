3c600ac660198bda9ee98fafe8ca3f5b50346ad0
indrodev <pstar3591@gmail.com>
2024-12-11 12:50:38 +0700

    menambahkan code pada folder/file Filters/AuthFilter.php dari baris 1-23

---
+++ app/Filters/AuthFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class AuthFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!session()->has('user')) {
+            // Redirect to login if user is not authenticated
+            return redirect()->to('/login')->with('error', 'Anda harus login terlebih dahulu.');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // No action needed after the request
+    }
+}
