e0a8f3e95573fd96120fe4989215a8b5ea353457
indrodev <pstar3591@gmail.com>
2024-12-11 13:10:51 +0700

    merubah code pada folder/file Controller/Dashboard.php pada baris ke 9-10

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,8 +6,8 @@ class Dashboard extends BaseController
 {
     public function index()
     {
-        if (!session()->get('isLoggedIn')) {
-            return redirect()->to('/login');
+        if (!session()->has('user')) {
+            return redirect()->to('/login')->with('error', 'Anda harus login terlebih dahulu.');
         }
 
         return view('layouts/index', ['username' => session()->get('username')]);
