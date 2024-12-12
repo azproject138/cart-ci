a25c733b40ab11550322b796c3d7135ab6701365
indrodev <pstar3591@gmail.com>
2024-12-12 11:25:19 +0700

    menambahkan code css pada folder/file partials/alerts.php baris 11

--- app/Views/partials/alerts.php
+++ app/Views/partials/alerts.php
@@ -8,6 +8,6 @@
 <?php if (session()->getFlashdata('error')): ?>
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <?= session()->getFlashdata('error') ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="btn-close" style="font-size: 15px;" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 <?php endif; ?>
