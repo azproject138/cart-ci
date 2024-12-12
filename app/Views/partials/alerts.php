2d143b6c3356b74daac915f80d7536e4cab99b38
indrodev <pstar3591@gmail.com>
2024-12-12 11:17:04 +0700

    menghapus code css pada folder/file partials/alerts.php baris 1-21

--- app/Views/partials/alerts.php
+++ app/Views/partials/alerts.php
@@ -1,24 +1,3 @@
-<style>
-    
-    .alert-danger {
-        margin: 0 0 0;
-        font-size: 15px;
-        color: red;
-        background: none;
-        border: none;
-        margin-bottom: 0;
-        margin-top: 0;
-        margin-left: 0;
-        margin-right: 0;
-        display: absolute;
-        flex-direction: column;
-        align-items: left;
-        text-align: left;
-        justify-content: lefcentert;
-    }
-
-</style>
-
 <?php if (session()->getFlashdata('success')): ?>
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <?= session()->getFlashdata('success') ?>
