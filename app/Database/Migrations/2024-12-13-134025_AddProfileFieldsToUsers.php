989ceaec3bdb75a56adff380d2768dc50ab2b560
indrodev <pstar3591@gmail.com>
2024-12-13 20:43:25 +0700

    menambahkan kode database pada file AddProfileFieldsToUsers.php

--- app/Database/Migrations/2024-12-13-134025_AddProfileFieldsToUsers.php
+++ app/Database/Migrations/2024-12-13-134025_AddProfileFieldsToUsers.php
@@ -9,10 +9,32 @@ class AddProfileFieldsToUsers extends Migration
     public function up()
     {
         //
+        $this->forge->addColumn('users', [
+            'profile_picture' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+            'address' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'whatsapp_number' => [
+                'type' => 'VARCHAR',
+                'constraint' => '15',
+                'null' => true,
+            ],
+            'otp_code' => [
+                'type' => 'VARCHAR',
+                'constraint' => '6',
+                'null' => true,
+            ],
+        ]);
     }
 
     public function down()
     {
         //
+        $this->forge->dropColumn('users', ['profile_picture', 'address', 'whatsapp_number', 'otp_code']);
     }
 }
