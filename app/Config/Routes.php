b15a78c1a15ba8c728e8333ec3553d9a6bd5e14a
indrodev <pstar3591@gmail.com>
2024-12-13 11:11:38 +0700

    menambahkan kode di file Routes.php baris 14-19

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,9 @@ $routes->get('/login', 'Auth::login');
 $routes->post('/login', 'Auth::processLogin');
 $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
 $routes->get('/logout', 'Auth::logout');
+
+$routes->get('/profile', 'ProfileController::index');
+$routes->post('/profile/update-picture', 'ProfileController::updateProfilePicture');
+$routes->post('/profile/update-address', 'ProfileController::updateAddress');
+$routes->post('/profile/update-whatsapp', 'ProfileController::updateWhatsApp');
+$routes->post('/profile/verify-otp', 'ProfileController::verifyOtp');
