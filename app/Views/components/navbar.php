0d9cef592711a022c2aca62edb5316be6538d2d6
indrodev <pstar3591@gmail.com>
2024-12-14 08:29:52 +0700

    mengubah kode pada file navbar.php baris ke 1-20

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,15 +1,19 @@
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="<?= base_url('uploads/' . session('user')['profile_picture'] ?? 'default.png') ?>" alt="Profile" class="rounded-circle" width="30" height="30">
-        <?= session('user')['username'] ?>
-        <?php if (!session('user')['profile_picture'] || !session('user')['address'] || !session('user')['whatsapp_number']): ?>
-            <span class="text-danger">•</span>
-        <?php endif; ?>
-    </a>
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-        <li><a class="dropdown-item" href="/profile">Profile</a></li>
-        <li><a class="dropdown-item" href="/settings">Settings</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item text-danger" href="/logout">Logout</a></li>
-    </ul>
-</li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">MyApp</a>
+        <div class="dropdown">
+            <a href="#" class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="<?= base_url('uploads/profile_pictures/' . ($user['profile_picture'] ?? 'default.png')) ?>" alt="Profile" class="rounded-circle" style="width: 40px; height: 40px;">
+                <?= $user['username'] ?>
+                <?php if (!$user['profile_picture']): ?>
+                    <span class="badge bg-danger">•</span>
+                <?php endif; ?>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                <li><a class="dropdown-item" href="/settings">Settings</a></li>
+                <li><a class="dropdown-item text-danger" href="/logout">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
