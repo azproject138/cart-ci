1fbba19db44f4dc3ba35904726d7142f17c53372
indrodev <pstar3591@gmail.com>
2024-12-14 08:42:39 +0700

    mengubah dan menambahkan kode pada file controller Home.php baris 5-6 dan 11-17

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $user = null;
+        if (session()->has('user_id')) {
+            $userModel = new UserModel();
+            $user = $userModel->find(session('user_id'));
+        }
+
+        return view('home', ['user' => $user]);
     }
 }
