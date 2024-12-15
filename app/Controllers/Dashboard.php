d8d8a814a311f45eda0032646784ed575d71e6d3
indrodev <pstar3591@gmail.com>
2024-12-15 17:22:49 +0700

    restore kode controller pada dropdown menu profile pada file Dashboard.php baris 18-22

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -15,4 +15,9 @@ class Dashboard extends BaseController
         return view('layouts/main', ['username' => session()->get('username')]);
     }
 
+    public function index()
+    {
+        return view('profile/index', ['username' => session()->get('username')]);
+    }
+
 }
