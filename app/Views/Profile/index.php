f29732496dd02143439a6c1adbcc9ab3d8e1fb55
indrodev <pstar3591@gmail.com>
2024-12-15 17:14:38 +0700

    upload foto profile

--- app/Views/Profile/index.php
+++ app/Views/Profile/index.php
@@ -14,7 +14,7 @@
     <form action="/profile/update-address" method="POST">
         <div class="mb-3">
             <label for="address" class="form-label">Alamat</label>
-            <input type="text" class="form-control" id="address" name="address" value="<?= $user['address'] ?>">
+            <input type="text" class="form-control" id="address" name="address" value="<?= isset($user['address']) ? $user['address'] : '' ?>">
         </div>
         <button type="submit" class="btn btn-primary">Simpan</button>
     </form>
@@ -23,7 +23,7 @@
     <form action="/profile/update-whatsapp" method="POST">
         <div class="mb-3">
             <label for="whatsapp" class="form-label">Nomor WhatsApp</label>
-            <input type="text" class="form-control" id="whatsapp" name="whatsapp" value="<?= $user['whatsapp'] ?>">
+            <input type="text" class="form-control" id="whatsapp" name="whatsapp" value="<?= isset($user['whatsapp']) ? $user['whatsapp'] : '' ?>">
         </div>
         <button type="submit" class="btn btn-primary">Kirim OTP</button>
     </form>
