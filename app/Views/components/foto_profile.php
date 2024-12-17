57686fc96a7781892d6526a99683488076339b6a
indrodev <pstar3591@gmail.com>
2024-12-17 11:15:58 +0700

    menambahkan atribut data-bs-toggle untuk menghubungkan elemen btn edit ke modal bootstrap,dan menambahkan atribut data-bs-datget

--- app/Views/components/foto_profile.php
+++ app/Views/components/foto_profile.php
@@ -7,9 +7,7 @@
     <div class="d-flex justify-content-center">
         <div class="position-relative">
             <img src="<?= base_url('uploads/profile_pictures/' . ($user['profile_picture'] ?? 'default.png')) ?>" alt="Profile Picture" class="rounded-circle" style="width: 100px; height: 100px;">
-            <button class="btn mt-3" data-bs-toggle="modal" data-bs-target="#uploadModal">
-                <i class="bi bi-pencil-fill position-absolute top-0 end-0 translate-middle p-1 bg-light rounded-circle" style="cursor: pointer;"></i>
-            </button>
+            <i class="bi bi-pencil-fill position-absolute top-0 end-0 translate-middle p-1 bg-light rounded-circle" data-bs-toggle="modal" data-bs-target="#uploadModal" style="cursor: pointer;"></i>
         </div>
     </div>
 
