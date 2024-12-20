45dc1477ba96474e394a099cf022727a55bd6082
indrodev <pstar3591@gmail.com>
2024-12-20 09:48:26 +0700

    menambahkan kode scrit pada file opsi_alamat_pengguna.js baris 1-13

---
+++ public/assets/opsi_alamat_pengguna.js
@@ -0,0 +1,13 @@
+<script>
+    const addressModal = document.getElementById('addressModal');
+    const addressTypeInput = document.getElementById('addressType');
+    const mapsLink = document.getElementById('mapsLink');
+
+    addressModal.addEventListener('show.bs.modal', (event) => {
+        const button = event.relatedTarget;
+        const addressType = button.getAttribute('data-address-type');
+        addressTypeInput.value = addressType;
+        const mapsQuery = addressType === 'home' ? 'Rumah' : 'Kantor';
+        mapsLink.href = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(mapsQuery)}`;
+    });
+</script>
