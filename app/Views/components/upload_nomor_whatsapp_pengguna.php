0c7311a3a894d7c8757d90db9567dcdf1494b156
indrodev <pstar3591@gmail.com>
2024-12-20 14:05:50 +0700

    modified kode pemanggilan route upload-nomor-whatsapp pada file upload_nomor_whatsapp_pengguna.php baris 12

--- app/Views/components/upload_nomor_whatsapp_pengguna.php
+++ app/Views/components/upload_nomor_whatsapp_pengguna.php
@@ -9,7 +9,7 @@
                 value="<?= $user['whatsapp_number'] ?? 'Belum diatur' ?>" 
                 disabled 
             />
-            <a href="<?= base_url('whatsapp/update') ?>" class="btn btn-primary">
+            <a href="<?= base_url('whatsapp/upload-nomor-whatsapp') ?>" class="btn btn-primary">
                 <i class="bi bi-pencil"></i> Edit
             </a>
         </div>
