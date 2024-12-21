2630c57ec124a6c001f3cbe32572c982c79563f2
indrodev <pstar3591@gmail.com>
2024-12-21 10:13:29 +0700

    add kode untuk memanggil components alerts.php baris 13, add kode pemanggilan file durasi_waktu_kode_otp_pengguna.js pada baris 20

--- app/Views/Profile/index.php
+++ app/Views/Profile/index.php
@@ -10,11 +10,13 @@
 <body>
     <div class="container mt-4">
         <h2>Profile</h2>
+        <?= $this->include('components/alerts')?>
         <?= view('components/foto_profile', ['user' => $user]) ?>
         <?= view('components/upload_alamat_pengguna', ['user' => $user]) ?>
         <?= view('components/upload_nomor_whatsapp_pengguna', ['user' => $user]) ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/durasi_waktu_kode_otp_pengguna.js"></script>
 </body>
 </html>
