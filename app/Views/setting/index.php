de347fdf359e8573a7cca46fb0646fbb5de40c33
indrodev <pstar3591@gmail.com>
2024-12-21 10:21:41 +0700

    add kode html, menambahkan link icon bootstrap baris 6, add link css bootstrap baris 7, add bundle bootstrap 14, add class container and title h2

---
+++ app/Views/setting/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Setting</title>
+</head>
+<body>
+    <div class="container mt-4"></div>
+    <h2>Setting</h2>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
