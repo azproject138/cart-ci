4f1c6b9ae2d45227d79af2f1fd176bdb7c7b832b
indrodev <pstar3591@gmail.com>
2024-12-21 10:50:04 +0700

    add kode pemanggilan file components edit_username_pengguna pada folder/file setting/index.php

--- app/Views/setting/index.php
+++ app/Views/setting/index.php
@@ -11,6 +11,7 @@
     <div class="container mt-4"></div>
     <h2>Setting</h2>
 
+    <?= $this->include('components/edit_username_pengguna')?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
