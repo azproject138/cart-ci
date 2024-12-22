1e649007960e417f9d044f7216099a09c85422bd
indrodev <pstar3591@gmail.com>
2024-12-22 13:37:35 +0700

    update kode pada file edit_username_pengguna.php

--- app/Views/components/edit_username_pengguna.php
+++ app/Views/components/edit_username_pengguna.php
@@ -4,14 +4,7 @@
     <div class="form-group">
         <label for="username">Username</label>
         <div class="input-group">
-            <input 
-                type="text" 
-                class="form-control" 
-                id="username" 
-                name="username" 
-                value="<?= esc($session->get('username')) ?>" 
-                readonly
-            >
+            <input type="text" class="form-control" name="username" value="<?= $user['username'] ?>" disabled>
             <button 
                 type="button" 
                 class="btn btn-secondary" 
@@ -24,22 +17,21 @@
     </div>
 </form>
 
-<div class="modal fade" id="editUsernameModal" tabindex="-1" aria-labelledby="editUsernameModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+<div class="modal fade" id="editUsernameModal" tabindex="-1" role="dialog" aria-labelledby="editUsernameModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <form action="settings/upload-username-pengguna" method="post">
+            <form action="/settings/update-username" method="post">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editUsernameModalLabel">Edit Username</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
-                    <div class="form-group">
-                        <label for="newUsername">New Username</label>
-                        <input type="text" class="form-control" id="newUsername" name="new_username" required>
-                    </div>
+                    <input type="text" class="form-control" name="username" value="<?= $user['username'] ?>" required>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
             </form>
