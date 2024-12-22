1cb851e6e03639c65d2673abff347ae858aa0fcf
indrodev <pstar3591@gmail.com>
2024-12-22 13:45:13 +0700

    add kode pada file edit_password_pengguna.php

---
+++ app/Views/components/edit_password_pengguna.php
@@ -0,0 +1,26 @@
+<div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" name="password">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editPasswordModal">Change Password</button>
+</div>
+<div class="modal fade" id="editPasswordModal" tabindex="-1" role="dialog" aria-labelledby="editPasswordModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="/settings/update-password" method="post">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editPasswordModalLabel">Change Password</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <input type="password" class="form-control" name="password" required>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
