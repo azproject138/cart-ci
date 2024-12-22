7f0bf02fbd55642ceb798825f2e0eabfa514dd3c
indrodev <pstar3591@gmail.com>
2024-12-22 13:41:03 +0700

    add kode pada file verifykasi_email_pengguna.php

---
+++ app/Views/components/verifykasi_email_pengguna.php
@@ -0,0 +1,22 @@
+<!-- Modal Verify Email -->
+<?php if ($user['email_verified'] == 0): ?>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#verifyEmailModal">Verify Email</button>
+    <div class="modal fade" id="verifyEmailModal" tabindex="-1" role="dialog" aria-labelledby="verifyEmailModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="verifyEmailModalLabel">Verify Email</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Please verify your email by clicking on the verification link sent to your email address.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
