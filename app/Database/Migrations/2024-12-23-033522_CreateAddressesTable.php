2587a3b087eacea22886007ec0f87d4f5f42513b
indrodev <pstar3591@gmail.com>
2024-12-23 10:36:35 +0700

    add file database CreateAdressesTable.php

---
+++ app/Database/Migrations/2024-12-23-033522_CreateAddressesTable.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'address_type' => [
+                'type' => 'ENUM',
+                'constraint' => ['rumah', 'kantor'],
+                'default' => 'rumah',
+            ],
+            'address_text' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('addresses');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('addresses');
+    }
+}
