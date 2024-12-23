aeb5cf55e2b1ba068b285eab5f01a364dd09f5bc
indrodev <pstar3591@gmail.com>
2024-12-23 09:15:18 +0700

    modified atribut class modal editPasswordModal and atribut close modal pada file edit_password_pengguna.php baris 14,19-20,28

--- app/Views/components/edit_password_pengguna.php
+++ app/Views/components/edit_password_pengguna.php
@@ -11,13 +11,13 @@
         </div>
     </div>
 </form>
-<div class="modal fade" id="editPasswordModal" tabindex="-1" role="dialog" aria-labelledby="editPasswordModalLabel" aria-hidden="true">
+<div class="modal fade" id="editPasswordModal" data-bs-backdrop="password" data-bs-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="editPasswordModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <form action="/settings/update-password-pengguna" method="post">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editPasswordModalLabel">Change Password</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <h5 class="modal-title" id="editPasswordModalLabel">Edit Password</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
@@ -25,7 +25,7 @@
                     <input type="password" class="form-control" name="password" required>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
             </form>
