1693d48c98b90d363fc28270330b5d50953e5437
indrodev <pstar3591@gmail.com>
2024-12-23 10:07:08 +0700

    modified pemanggilan route controller saveAlamatPengguna.php pada file upload_alamat_pengguna.php baris 45

--- app/Views/components/upload_alamat_pengguna.php
+++ app/Views/components/upload_alamat_pengguna.php
@@ -42,7 +42,7 @@
                 <h5 class="modal-title" id="addressModalLabel">Edit Alamat</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="<?= base_url('address/save') ?>" method="post">
+            <form action="<?= base_url('simpan/alamat-pengguna') ?>" method="post">
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="address_type" class="form-label">Tipe Alamat</label>
