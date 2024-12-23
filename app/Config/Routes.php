18ff84305aa7eef955ab912259f2f509dd03246c
indrodev <pstar3591@gmail.com>
2024-12-23 10:02:37 +0700

    rapikan kode pada file Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,17 +14,14 @@ $routes->get('/logout', 'Auth::logout');
 
 $routes->get('/profile', 'Dashboard::index');
 $routes->post('/profile/upload-picture', 'Dashboard::uploadPicture');
-
 $routes->post('simpan/alamat', 'Dashboard::saveAlamatPengguna');
 $routes->get('upload-alamat-pengguna', 'Dashboard::viewAlamatPengguna');
-
 $routes->get('whatsapp/upload-nomor-whatsapp', 'Dashboard::uploadNomorWhatsApp');
 $routes->post('whatsapp/send-kode-otp', 'Dashboard::sendKodeOTP');
 $routes->post('whatsapp/verify-kode-otp', 'Dashboard::verifyKodeOtp');
 
 $routes->get('/settings', 'SettingController::index', ['filter' => 'auth']);
 $routes->post('settings/update-username-pengguna', 'SettingController::updateUsernamePengguna');
-
 $routes->post('settings/update-email-pengguna', 'SettingController::updateEmailPengguna');
 $routes->get('/settings/verify-email-pengguna', 'SettingController::verifyEmailPengguna');
 $routes->post('settings/update-email-pengguna', 'SettingController::updateEmailPengguna');
