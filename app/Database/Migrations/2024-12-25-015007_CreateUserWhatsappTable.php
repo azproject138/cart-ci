9671b714a7a8182e2510e30c42fa9267bdac5c65
indrodev <pstar3591@gmail.com>
2024-12-25 08:51:00 +0700

    add file database migration CreateUserWhatsappTable

---
+++ app/Database/Migrations/2024-12-25-015007_CreateUserWhatsappTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserWhatsappTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
