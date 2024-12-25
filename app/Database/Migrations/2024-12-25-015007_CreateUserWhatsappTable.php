fbaf93859c0ce1add40a52c564794cce468b9024
indrodev <pstar3591@gmail.com>
2024-12-25 09:19:37 +0700

    modified:   .env         deleted:    app/Database/Migrations/2024-11-20-054803_TabelDatabaseUser. php         deleted:    app/Database/Migrations/2024-12-24-104245_CreateWhatsappNumb ersTable.php         modified:   app/Database/Migrations/2024-12-25-015007_CreateUserWhatsapp Table.php         modified:   writable/logs/log-2024-12-25.log, app/Database/Migrations/2024-11-20-054803_CreateUsersTable.php

--- app/Database/Migrations/2024-12-25-015007_CreateUserWhatsappTable.php
+++ app/Database/Migrations/2024-12-25-015007_CreateUserWhatsappTable.php
@@ -4,15 +4,44 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class CreateUserWhatsappTable extends Migration
+class CreateUserWhatsAppTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+            ],
+            'whatsapp_number' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+            ],
+            'is_primary' => [
+                'type'       => 'BOOLEAN',
+                'default'    => false,
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('user_whatsapp');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user_whatsapp');
     }
 }
