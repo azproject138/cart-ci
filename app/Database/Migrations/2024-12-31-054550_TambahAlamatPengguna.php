4175fcf144174490c865380392ec9798cdbcb898
indrodev <pstar3591@gmail.com>
2024-12-31 13:02:28 +0700

    add kode database TambahAlamatPengguna baris 11-29,32, modified:   writable/logs/log-2024-12-31.log

--- app/Database/Migrations/2024-12-31-054550_TambahAlamatPengguna.php
+++ app/Database/Migrations/2024-12-31-054550_TambahAlamatPengguna.php
@@ -8,11 +8,27 @@ class TambahAlamatPengguna extends Migration
 {
     public function up()
     {
-        //
+        $fields = [
+            'alamat' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'tipe_alamat' => [
+                'type' => 'ENUM',
+                'constraint' => ['home', 'office'],
+                'default' => 'home',
+            ],
+            'alamat_utama' => [
+                'type' => 'BOOLEAN',
+                'default' => false,
+            ],
+        ];
+
+        $this->forge->addColumn('users', $fields);
     }
 
     public function down()
     {
-        //
+        $this->forge->dropColumn('users', ['alamat', 'tipe_alamat', 'alamat_utama']);
     }
 }
