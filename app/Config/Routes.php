29eda2a9956c733f53d9502891e23ff78fbcf03f
indrodev <pstar3591@gmail.com>
2025-01-02 15:31:34 +0700

    modified:   app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,8 @@ $routes->post('/login', 'Auth::processLogin');
 $routes->get('/dashboard', 'Dashboard::main', ['filter' => 'auth']);
 $routes->get('/logout', 'Auth::logout');
 
+//dropdown menu profile 
+//profile pengguna
 $routes->get('/profile', 'ProfilePenggunaController::index');
 $routes->post('profile/upload-profile-pengguna', 'ProfilePenggunaController::uploadProfilePengguna');
 $routes->get('profile/delete-profile-pengguna', 'ProfilePenggunaController::deleteProfilePengguna/$1');
@@ -21,11 +23,13 @@ $routes->get('/alamat', 'AlamatPenggunaController::index');
 $routes->post('/alamat/tambah-alamat-pengguna', 'AlamatPenggunaController::tambahAlamatPengguna');
 $routes->get('/alamat/hapus-alamat-pengguna', 'AlamatPenggunaController::hapusAlamatPengguna');
 
+//nomor whatsapp pengguna
 $routes->get('/whatsapp', 'UserWhatsappController::index');
 $routes->post('/whatsapp/tambah-whatsapp-pengguna', 'UserWhatsappController::tambahWhatsAppPengguna');
 $routes->post('/whatsapp/edit-whatsapp-pengguna', 'UserWhatsappController::editWhatsAppPengguna');
 $routes->get('/whatsapp/hapus-whatsapp-pengguna', 'UserWhatsappController::hapuseWhatsAppPengguna');
 
+//doropdown menu setting
 $routes->get('/settings', 'SettingController::index', ['filter' => 'auth']);
 $routes->post('settings/update-username-pengguna', 'SettingController::updateUsernamePengguna');
 $routes->post('settings/update-email-pengguna', 'SettingController::updateEmailPengguna');
