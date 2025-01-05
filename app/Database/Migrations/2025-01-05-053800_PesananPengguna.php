44d7702b768693e864e9f803fd9cf91e7f1050d9
indrodev <pstar3591@gmail.com>
2025-01-05 14:05:03 +0700

    add kode tabel database migration id,jenis_pesanan,merek_pesanan,kategori_pesanan,ketentuan_pesanan,jumlah_pesanan,deskripsi_pesanan,created_at,updated_at,addKey('id', true)baris 56,createTable('pesanan_pengguna')baris 57,dropTable('pesanan_pengguna')baris 62 pada file PesananPengguna.php,// add kode database migration user_id baris 12,alamat baris 19,whatsapp_number baris 24,addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE')baris 33, dropForeignKey('pesanan_pengguna', 'order_user_id_foreign')baris 38,dropColumn('pesanan_pengguna', ['user_id', 'alamat', 'whatsapp_number'])baris 39 pada AddTabelAlamatDanWhatsappPengguna.php untuk mengkoneksikan alamat dan whatsapp_number ke tabel user// deleted:    app/Database/Migrations/2025-01-05-054803_TabelJenisPesananP engguna.php         deleted:    app/Database/Migrations/2025-01-05-054842_TabelMerekPesananP engguna.php         deleted:    app/Database/Migrations/2025-01-05-054857_TabelKategoriPesan anPengguna.php

--- app/Database/Migrations/2025-01-05-053800_PesananPengguna.php
+++ app/Database/Migrations/2025-01-05-053800_PesananPengguna.php
@@ -8,11 +8,57 @@ class PesananPengguna extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'jenis_pesanan' => [
+                'type'       => 'ENUM',
+                'constraint' => ['jenis pesanan', 'Servis Laptop', 'Servis Printer', 'Pasang WiFi', 'Pasang CCTV'],
+                'default'    => 'jenis pesanan',
+            ],
+            'merek_pesanan' => [
+                'type'       => 'ENUM',
+                'constraint' => ['merek pesanan', 'HP', 'Asus', 'Epson', 'Canon', 'TP-Link', 'Hikvision'],
+                'default'    => 'merek pesanan',
+            ],
+            'kategori_pesanan' => [
+                'type'       => 'ENUM',
+                'constraint' => ['kategori pesanan','install ulang', 'mati total', 'revil toner', 'ganti tinta', 'order wifi', 'order cctv'],
+                'default'    => 'kategori pesanan',
+            ],
+            'ketentuan_servis' => [
+                'type'       => 'ENUM',
+                'constraint' => ['ketentuan servis', 'ambil', 'antar'],
+                'default'    => 'ketentuan servis',
+            ],
+            'jumlah_pesanan' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'deskripsi_pesanan' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('pesanan_pengguna');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('pesanan_pengguna');
     }
 }
--- app/Database/Migrations/2025-01-05-055429_AddTabelAlamatDanWhatsappPengguna.php
+++ app/Database/Migrations/2025-01-05-055429_AddTabelAlamatDanWhatsappPengguna.php
@@ -8,11 +8,34 @@ class AddTabelAlamatDanWhatsappPengguna extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addColumn('orders', [
+            'user_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => false,
+                'after'      => 'id',
+            ],
+            'alamat' => [
+                'type' => 'TEXT',
+                'null' => true,
+                'after' => 'deskripsi_pesanan',
+            ],
+            'whatsapp_number' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+                'null'       => true,
+                'after'      => 'alamat',
+            ],
+        ]);
+
+        // Tambahkan foreign key untuk user_id
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropForeignKey('pesanan_pengguna', 'orders_user_id_foreign');
+        $this->forge->dropColumn('pesanan_pengguna', ['user_id', 'alamat', 'whatsapp_number']);
     }
 }
