68a2f74411a21231e43b01b0e8a73bd280e8c87c
indrodev <pstar3591@gmail.com>
2025-01-05 12:02:56 +0700

    add file components sidebar.php, add kode pemanggilan components sidebar pada file main baris 18, modified css button open sidebar pada file css baris 253, deleted:    writable/debugbar/debugbar_1735819221.585852.json         deleted:    writable/debugbar/debugbar_1735819221.960786.json         deleted:    writable/debugbar/debugbar_1735819365.471077.json         deleted:    writable/debugbar/debugbar_1735819366.150434.json         deleted:    writable/debugbar/debugbar_1735819369.471880.json         deleted:    writable/debugbar/debugbar_1735819393.141277.json         deleted:    writable/debugbar/debugbar_1735819393.866723.json         deleted:    writable/debugbar/debugbar_1735819566.850039.json         deleted:    writable/debugbar/debugbar_1735819567.142327.json         deleted:    writable/debugbar/debugbar_1735819573.194098.json         new file:   writable/debugbar/debugbar_1736052980.151643.json         new file:   writable/debugbar/debugbar_1736053015.411573.json         new file:   writable/debugbar/debugbar_1736053016.083616.json         new file:   writable/debugbar/debugbar_1736053057.486804.json         new file:   writable/debugbar/debugbar_1736053085.002919.json         new file:   writable/debugbar/debugbar_1736053085.560470.json         new file:   writable/debugbar/debugbar_1736053193.880720.json         new file:   writable/debugbar/debugbar_1736053194.260335.json         new file:   writable/debugbar/debugbar_1736053201.892089.json         new file:   writable/debugbar/debugbar_1736053202.176438.json         new file:   writable/logs/log-2025-01-05.log         new file:   writable/session/ci_sessionv5efggotqmjkbinjlf2ehsrr0518ik5i         new file:   writable/session/ci_sessionvtv2sm9d271e3d41v28jfqfga3a6ubm4

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,35 @@
+<nav>
+    <!--sidebar-->
+    <div id="sidebar" class="sidebar">
+        <button id="close-btn" class="close-btn"><i class="bi bi-x"></i></button>
+        <ul>
+            <li>
+                <a href="#">
+                    <img src="/assets/img/dashboard.png" alt="dashboard">Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="/assets/img/history.png" alt="history">Riwayat
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="/assets/img/operator.png" alt="operator">Contact
+                </a>
+            </li>
+        </ul>
+
+        
+
+        <!--Footer-->
+        <div class="footer" style="width: 100%; text-align: center; padding: 10px; color: white; font-size: 12px; margin-top: 150px;">
+            <span>Copyright &copy; 2024</span>
+        </div>
+
+        <div class="footer-sidebar">
+            <hr>
+            <div> Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
+        </div>
+    </div>
+</nav>
