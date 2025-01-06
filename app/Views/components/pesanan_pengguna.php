be10c491b2569cc31f722a247e6713ddeb285299
indrodev <pstar3591@gmail.com>
2025-01-06 14:21:10 +0700

    add file pesanan_pengguna.php pada folder components

---
+++ app/Views/components/pesanan_pengguna.php
@@ -0,0 +1,8 @@
+<?= $this->extends('layouts/main') ?>
+<?= $this->section('content'); ?>
+
+<button id="tambah-pesanan" class="tambah-pesanan">
+    <i class="bi bi-cart-plus-fill"></i> Tambah Pesanan
+</button>
+
+<?= $this->endSection(); ?>
