9b52e1fc4706d12d5dee6242a51a893a26e77a38
indrodev <pstar3591@gmail.com>
2025-01-09 15:15:10 +0700

    modified:   app/Views/components/pesanan_pengguna.php         modified:   public/assets/tabelPengguna.css         new file:   public/assets/tambah_pesanan_pengguna.css//deleted:    writable/debugbar/debugbar_1736407378.735271.json         deleted:    writable/debugbar/debugbar_1736407379.222242.json         deleted:    writable/debugbar/debugbar_1736407387.171004.json         deleted:    writable/debugbar/debugbar_1736407387.553664.json         deleted:    writable/debugbar/debugbar_1736407413.777113.json         deleted:    writable/debugbar/debugbar_1736407414.258037.json         deleted:    writable/debugbar/debugbar_1736407420.048731.json         deleted:    writable/debugbar/debugbar_1736407420.487009.json         deleted:    writable/debugbar/debugbar_1736407420.842895.json         deleted:    writable/debugbar/debugbar_1736407437.715852.json         deleted:    writable/debugbar/debugbar_1736407438.088391.json         deleted:    writable/debugbar/debugbar_1736407443.690802.json         deleted:    writable/debugbar/debugbar_1736407444.122384.json         deleted:    writable/debugbar/debugbar_1736407466.208284.json         deleted:    writable/debugbar/debugbar_1736407466.553809.json         deleted:    writable/debugbar/debugbar_1736407473.090006.json         deleted:    writable/debugbar/debugbar_1736407473.581350.json         deleted:    writable/debugbar/debugbar_1736407476.830683.json         deleted:    writable/debugbar/debugbar_1736407477.204094.json         deleted:    writable/debugbar/debugbar_1736407500.143070.json         new file:   writable/debugbar/debugbar_1736410324.995297.json         new file:   writable/debugbar/debugbar_1736410333.129649.json         new file:   writable/debugbar/debugbar_1736410333.598426.json         new file:   writable/debugbar/debugbar_1736410345.741028.json         new file:   writable/debugbar/debugbar_1736410346.094109.json         new file:   writable/debugbar/debugbar_1736410354.754571.json         new file:   writable/debugbar/debugbar_1736410387.789081.json         new file:   writable/debugbar/debugbar_1736410388.065580.json         new file:   writable/debugbar/debugbar_1736410405.641219.json         new file:   writable/debugbar/debugbar_1736410405.967622.json         new file:   writable/debugbar/debugbar_1736410413.862930.json         new file:   writable/debugbar/debugbar_1736410414.132191.json         new file:   writable/debugbar/debugbar_1736410417.752866.json         new file:   writable/debugbar/debugbar_1736410418.008283.json         new file:   writable/debugbar/debugbar_1736410420.632203.json         new file:   writable/debugbar/debugbar_1736410420.879878.json         new file:   writable/debugbar/debugbar_1736410424.319042.json         new file:   writable/debugbar/debugbar_1736410424.584196.json         new file:   writable/debugbar/debugbar_1736410444.780617.json         new file:   writable/debugbar/debugbar_1736410449.603003.json         modified:   writable/logs/log-2025-01-09.log         modified:   writable/session/ci_session5dpnb09uakr261gq7e0cn4e54pg7noav         new file:   writable/session/ci_session5k1s52fgpnp9g2u8vpvnv5f6hgpilthm         new file:   writable/session/ci_session6vbf70s5c8qt5v5ja2fesn3lp3kg95dg         new file:   writable/session/ci_session777nq5cus3ppc4om26qliqknfnqs66sr         new file:   writable/session/ci_sessionn1drccfabi0o9vppn07k78n34k1abnst         new file:   writable/session/ci_sessionqjcq0m42if35u2ju2fn9a1j3s05811bo         new file:   writable/session/ci_sessionumureg82ltcfm7tfudlpi7dug91mfgli

--- app/Views/components/pesanan_pengguna.php
+++ app/Views/components/pesanan_pengguna.php
@@ -9,28 +9,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <title>Order Pesanan Pengguna</title>
+    <link rel="stylesheet" href="/assets/tambah_pesanan_pengguna.css">
+    <title>Pesanan Pengguna</title>
 </head>
 <body>
     <div class="container mt-5">
-        <h1 class="text-center">Tambah Pesanan</h1>
+        <h2 class="text-center">Tambah Pesanan</h2>
         <hr>
 
         <form action="<?= base_url('/pesanan/tambah-pesanan-pengguna'); ?>" method="post">
             <?= csrf_field(); ?>
 
-            <div class="mb-3 mt-3">
+            <div class="mb-3">
                 <label for="alamat">Alamat:</label>
                 <input type="text" class="form-control mt-2" name="alamat" id="alamat" value="<?= esc($user['alamat'] ?? '') ?>" readonly>
             </div>
-            <div class="mb-3 mt-3">
+            <div class="mb-3">
                 <label for="whatsapp_number">WhatsApp:</label>
                 <input type="text" class="form-control mt-2" name="whatsapp_number" id="whatsapp_number" value="<?= esc($user['whatsapp_number'] ?? '') ?>" readonly>
             </div>
 
-            <div class="mb-3 mt-2">
+            <div class="mb-3">
                 <label for="jenisPesanan" class="form-label">Jenis Pesanan</label>
-                <select id="jenisPesanan" name="jenis_pesanan" class="form-control" required>
+                <select id="jenisPesanan" name="jenis_pesanan" class="input form-control" required>
                     <option value="">Pilih Jenis Pesanan</option>
                     <option value="Servis Laptop">Servis Laptop</option>
                     <option value="Servis Printer">Servis Printer</option>
@@ -41,43 +42,43 @@
 
             <div class="mb-3">
                 <label for="merekPesanan" class="form-label">Merek Pesanan</label>
-                <select id="merekPesanan" name="merek_pesanan" class="form-control" required>
+                <select id="merekPesanan" name="merek_pesanan" class="input form-control" required>
                     <option value="">Pilih Merek</option>
                 </select>
             </div>
 
             <div class="mb-3">
                 <label for="kategoriPesanan" class="form-label">Kategori Pesanan</label>
-                <select id="kategoriPesanan" name="kategori_pesanan" class="form-control" required>
+                <select id="kategoriPesanan" name="kategori_pesanan" class="input form-control" required>
                     <option value="">Pilih Kategori</option>
                 </select>
             </div>
 
             <label for="ketentuan_servis">Ketentuan Servis:</label>
-            <select name="ketentuan_servis" class="form-control mt-2" id="ketentuan_servis">
+            <select name="ketentuan_servis" class="input form-control mt-2" id="ketentuan_servis">
                 <option value="Pilih Ketentuan">Pilih Ketentuan</option>
                 <option value="Antar">Antar</option>
                 <option value="Ambil">Ambil</option>
             </select>
 
-            <div class="mb-3 mt-3">
+            <div class="mb-3">
                 <label for="tanggal_pesanan">Tanggal Pesanan</label>
                 <input type="date" class="form-control mt-2" name="tanggal_pesanan" id="tanggal_pesanan" value="<?= set_value('tanggal_pesanan') ?>" required>
             </div>
 
-            <div class="mb-3 mt-3">
+            <div class="mb-3">
                 <label for="jumlahPesanan" class="form-label">Jumlah Pesanan</label>
                 <input type="number" id="jumlahPesanan" name="jumlah_pesanan" class="form-control" min="1" required>
             </div>
 
             <div class="mb-3">
                 <label for="deskripsiPesanan" class="form-label">Deskripsi Pesanan</label>
-                <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="form-control" rows="3" required></textarea>
+                <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="input form-control" rows="3" required></textarea>
             </div>
 
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">
-                    Simpan
+            <div class="mb-3 mt-3 d-flex justify-content-center align-items-center flex-column gap-2 text-center">
+                <button type="submit" class="btn btn-primary submit-btn">
+                    <i class="bi bi-cloud-download"></i> Simpan
                 </button>
             </div>
         </form>
