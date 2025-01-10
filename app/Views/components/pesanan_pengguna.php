69521d96d8680e75b3c3231c36d7d6b95052168a
indrodev <pstar3591@gmail.com>
2025-01-10 18:03:47 +0700

    modified:   app/Views/components/pesanan_pengguna.php         modified:   public/assets/tambah_pesanan_pengguna.css//deleted:    writable/debugbar/debugbar_1736410345.741028.json         deleted:    writable/debugbar/debugbar_1736410387.789081.json         deleted:    writable/debugbar/debugbar_1736410405.967622.json         deleted:    writable/debugbar/debugbar_1736410413.862930.json         deleted:    writable/debugbar/debugbar_1736410414.132191.json         deleted:    writable/debugbar/debugbar_1736410424.319042.json         deleted:    writable/debugbar/debugbar_1736410424.584196.json         deleted:    writable/debugbar/debugbar_1736410444.780617.json         renamed:    writable/debugbar/debugbar_1736410354.754571.json -> writabl e/debugbar/debugbar_1736506832.288266.json         renamed:    writable/debugbar/debugbar_1736410420.879878.json -> writabl e/debugbar/debugbar_1736506856.210266.json         new file:   writable/debugbar/debugbar_1736506856.633603.json         renamed:    writable/debugbar/debugbar_1736410417.752866.json -> writabl e/debugbar/debugbar_1736506864.732592.json         renamed:    writable/debugbar/debugbar_1736410418.008283.json -> writabl e/debugbar/debugbar_1736506865.036038.json         new file:   writable/debugbar/debugbar_1736506871.018288.json         new file:   writable/debugbar/debugbar_1736506871.425106.json         new file:   writable/debugbar/debugbar_1736506906.118077.json         renamed:    writable/debugbar/debugbar_1736410388.065580.json -> writabl e/debugbar/debugbar_1736506906.427150.json         renamed:    writable/debugbar/debugbar_1736410333.598426.json -> writabl e/debugbar/debugbar_1736506911.109577.json         renamed:    writable/debugbar/debugbar_1736410420.632203.json -> writabl e/debugbar/debugbar_1736506911.518699.json         renamed:    writable/debugbar/debugbar_1736410324.995297.json -> writabl e/debugbar/debugbar_1736506918.798530.json         renamed:    writable/debugbar/debugbar_1736410449.603003.json -> writabl e/debugbar/debugbar_1736506919.062446.json         renamed:    writable/debugbar/debugbar_1736410333.129649.json -> writabl e/debugbar/debugbar_1736506930.003837.json         renamed:    writable/debugbar/debugbar_1736410405.641219.json -> writabl e/debugbar/debugbar_1736506939.481523.json         renamed:    writable/debugbar/debugbar_1736410346.094109.json -> writabl e/debugbar/debugbar_1736506946.509471.json         new file:   writable/debugbar/debugbar_1736506951.033828.json         new file:   writable/debugbar/debugbar_1736506967.242405.json         new file:   writable/debugbar/debugbar_1736506972.342097.json         new file:   writable/debugbar/debugbar_1736506975.663207.json         new file:   writable/logs/log-2025-01-10.log         new file:   writable/session/ci_session0fcdfpi679d3qj29se3vm4s21ibimt9v         new file:   writable/session/ci_session5cplph6v0778i0l3uf9ht4b3b1l6k7q8         new file:   writable/session/ci_session9pnut5omacdvbun8bu78tcdf74n4hg2d         new file:   writable/session/ci_sessionas68f91k3ufadlhee4iluenrmgonq2t8         new file:   writable/session/ci_sessiondcqnr8vk8g09tfu95c212kja0psepk23         new file:   writable/session/ci_sessioneqv27edh1hs74b5en8a21fjlftnom1p0         new file:   writable/session/ci_sessionglrvko7hnth2302ee2ir333q0d29g29m         new file:   writable/session/ci_sessioni4h5nib8n1q9fnmc5fkaph8s8r3d22es         new file:   writable/session/ci_sessionjeigjdl1o0l5fr4i1sd4rgct3gph7f69         new file:   writable/session/ci_sessionrlkua250riok4ealemfakank54vr7fr1         new file:   writable/session/ci_sessionujtpbumh8ejd1crnccqf3sr2vg9tm29f         new file:   writable/session/ci_sessionvcc2r04ordsld87kt0ecodcm3mrblhk8

--- app/Views/components/pesanan_pengguna.php
+++ app/Views/components/pesanan_pengguna.php
@@ -13,75 +13,93 @@
     <title>Pesanan Pengguna</title>
 </head>
 <body>
-    <div class="container mt-5">
-        <h2 class="text-center">Tambah Pesanan</h2>
+    <div class="container">
+        <h2>Tambah Pesanan</h2>
         <hr>
 
-        <form action="<?= base_url('/pesanan/tambah-pesanan-pengguna'); ?>" method="post">
-            <?= csrf_field(); ?>
+        <div class="form-group">
+            <form action="<?= base_url('/pesanan/tambah-pesanan-pengguna'); ?>" method="post">
+                <?= csrf_field(); ?>
 
-            <div class="mb-3">
-                <label for="alamat">Alamat:</label>
-                <input type="text" class="form-control mt-2" name="alamat" id="alamat" value="<?= esc($user['alamat'] ?? '') ?>" readonly>
-            </div>
-            <div class="mb-3">
-                <label for="whatsapp_number">WhatsApp:</label>
-                <input type="text" class="form-control mt-2" name="whatsapp_number" id="whatsapp_number" value="<?= esc($user['whatsapp_number'] ?? '') ?>" readonly>
-            </div>
+                <div class="form-identitas">
+                    <div class="mb-3">
+                        <label for="alamat" class="form-label">Alamat:</label>
+                        <input type="text" class="form-control mt-1" name="alamat" id="alamat" value="<?= esc($user['alamat'] ?? '') ?>" readonly>
+                    </div>
+                    <div class="mb-3">
+                        <label for="whatsapp_number" class="form-label">WhatsApp:</label>
+                        <input type="text" class="form-control mt-1" name="whatsapp_number" id="whatsapp_number" value="<?= esc($user['whatsapp_number'] ?? '') ?>" readonly>
+                    </div>
+                </div>
 
-            <div class="mb-3">
-                <label for="jenisPesanan" class="form-label">Jenis Pesanan</label>
-                <select id="jenisPesanan" name="jenis_pesanan" class="input form-control" required>
-                    <option value="">Pilih Jenis Pesanan</option>
-                    <option value="Servis Laptop">Servis Laptop</option>
-                    <option value="Servis Printer">Servis Printer</option>
-                    <option value="Pasang WiFi">Pasang WiFi</option>
-                    <option value="Pasang CCTV">Pasang CCTV</option>
-                </select>
-            </div>
+                <div class="form-pesanan">
+                    <div class="mb-3">
+                        <label for="jenisPesanan" class="form-label">Jenis Pesanan</label>
+                        <select id="jenisPesanan" name="jenis_pesanan" class="input form-control" required>
+                            <option value="">Pilih Jenis Pesanan</option>
+                            <option value="Servis Laptop">Servis Laptop</option>
+                            <option value="Servis Printer">Servis Printer</option>
+                            <option value="Pasang WiFi">Pasang WiFi</option>
+                            <option value="Pasang CCTV">Pasang CCTV</option>
+                        </select>
+                    </div>
 
-            <div class="mb-3">
-                <label for="merekPesanan" class="form-label">Merek Pesanan</label>
-                <select id="merekPesanan" name="merek_pesanan" class="input form-control" required>
-                    <option value="">Pilih Merek</option>
-                </select>
-            </div>
+                    <div class="mb-3">
+                        <label for="merekPesanan" class="form-label">Merek Pesanan</label>
+                        <select id="merekPesanan" name="merek_pesanan" class="input form-control" required>
+                            <option value="">Pilih Merek</option>
+                        </select>
+                    </div>
 
-            <div class="mb-3">
-                <label for="kategoriPesanan" class="form-label">Kategori Pesanan</label>
-                <select id="kategoriPesanan" name="kategori_pesanan" class="input form-control" required>
-                    <option value="">Pilih Kategori</option>
-                </select>
-            </div>
+                    <div class="mb-3">
+                        <label for="kategoriPesanan" class="form-label">Kategori Pesanan</label>
+                        <select id="kategoriPesanan" name="kategori_pesanan" class="input form-control" required>
+                            <option value="">Pilih Kategori</option>
+                        </select>
+                    </div>
 
-            <label for="ketentuan_servis">Ketentuan Servis:</label>
-            <select name="ketentuan_servis" class="input form-control mt-2" id="ketentuan_servis">
-                <option value="Pilih Ketentuan">Pilih Ketentuan</option>
-                <option value="Antar">Antar</option>
-                <option value="Ambil">Ambil</option>
-            </select>
+                    <div class="mb-3">
+                        <label for="ketentuan_servis" class="form-label">Ketentuan Servis:</label>
+                        <select name="ketentuan_servis" class="input form-control" id="ketentuan_servis">
+                            <option value="Pilih Ketentuan">Pilih Ketentuan</option>
+                            <option value="Antar">Antar</option>
+                            <option value="Ambil">Ambil</option>
+                        </select>
+                    </div>
 
-            <div class="mb-3">
-                <label for="tanggal_pesanan">Tanggal Pesanan</label>
-                <input type="date" class="form-control mt-2" name="tanggal_pesanan" id="tanggal_pesanan" value="<?= set_value('tanggal_pesanan') ?>" required>
-            </div>
+                </div>
 
-            <div class="mb-3">
-                <label for="jumlahPesanan" class="form-label">Jumlah Pesanan</label>
-                <input type="number" id="jumlahPesanan" name="jumlah_pesanan" class="form-control" min="1" required>
-            </div>
+                <div class="form-ketentuan">
+                    <div class="mb-3">
+                        <label for="tanggal_pesanan" class="form-label">Tanggal Pesanan</label>
+                        <input type="date" class="form-control" name="tanggal_pesanan" id="tanggal_pesanan" value="<?= set_value('tanggal_pesanan') ?>" required>
+                    </div>
 
-            <div class="mb-3">
-                <label for="deskripsiPesanan" class="form-label">Deskripsi Pesanan</label>
-                <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="input form-control" rows="3" required></textarea>
-            </div>
+                    <div class="mb-3">
+                        <label for="jumlahPesanan" class="form-label">Jumlah Pesanan</label>
+                        <input type="number" id="jumlahPesanan" name="jumlah_pesanan" class="input form-control mt-1" min="1" required>
+                    </div>
+                </div>
 
-            <div class="mb-3 mt-3 d-flex justify-content-center align-items-center flex-column gap-2 text-center">
-                <button type="submit" class="btn btn-primary submit-btn">
-                    <i class="bi bi-cloud-download"></i> Simpan
-                </button>
-            </div>
-        </form>
+                <div class="form-deskripsi">
+                    <div class="mb-3">
+                        <label for="deskripsiPesanan" class="form-label">Deskripsi Pesanan</label>
+                        <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="input form-control mt-1" rows="3" required></textarea>
+                    </div>
+                </div>
+
+                <div class="mb-3 mt-3 d-flex justify-content-center align-items-center flex-column gap-2 text-center">
+                    <button type="submit" class="btn btn-primary submit-btn">
+                        <i class="bi bi-cloud-download"></i> Simpan
+                    </button>
+                    <button type="button" class="close-btn">
+                        <a href="/">
+                            <i class="bi bi-x"></i>
+                        </a>
+                    </button>
+                </div>
+            </form>
+        </div>
 
     </div>
     <script src="/assets/pesananPengguna.js"></script>
