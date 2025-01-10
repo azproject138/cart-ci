e7031217a23048ee4b089f83957fc8eff0f8b118
indrodev <pstar3591@gmail.com>
2025-01-10 20:01:55 +0700

    modified:   app/Views/components/pesanan_pengguna.php         modified:   public/assets/tambah_pesanan_pengguna.css//deleted:    writable/debugbar/debugbar_1736506856.210266.json         deleted:    writable/debugbar/debugbar_1736506871.018288.json         deleted:    writable/debugbar/debugbar_1736506906.427150.json         deleted:    writable/debugbar/debugbar_1736506911.518699.json         deleted:    writable/debugbar/debugbar_1736506919.062446.json         deleted:    writable/debugbar/debugbar_1736506946.509471.json         deleted:    writable/debugbar/debugbar_1736506972.342097.json         deleted:    writable/debugbar/debugbar_1736506975.663207.json         new file:   writable/debugbar/debugbar_1736513875.170924.json         renamed:    writable/debugbar/debugbar_1736506856.633603.json -> writabl e/debugbar/debugbar_1736513875.488315.json         new file:   writable/debugbar/debugbar_1736513894.853007.json         renamed:    writable/debugbar/debugbar_1736506871.425106.json -> writabl e/debugbar/debugbar_1736513895.217721.json         new file:   writable/debugbar/debugbar_1736513906.039334.json         renamed:    writable/debugbar/debugbar_1736506911.109577.json -> writabl e/debugbar/debugbar_1736513906.371975.json         renamed:    writable/debugbar/debugbar_1736506906.118077.json -> writabl e/debugbar/debugbar_1736513926.658573.json         renamed:    writable/debugbar/debugbar_1736506865.036038.json -> writabl e/debugbar/debugbar_1736513927.030968.json         new file:   writable/debugbar/debugbar_1736513940.672124.json         renamed:    writable/debugbar/debugbar_1736506967.242405.json -> writabl e/debugbar/debugbar_1736513950.354534.json         renamed:    writable/debugbar/debugbar_1736506939.481523.json -> writabl e/debugbar/debugbar_1736513950.784131.json         new file:   writable/debugbar/debugbar_1736513961.089202.json         renamed:    writable/debugbar/debugbar_1736506832.288266.json -> writabl e/debugbar/debugbar_1736513967.355528.json         renamed:    writable/debugbar/debugbar_1736506864.732592.json -> writabl e/debugbar/debugbar_1736513967.814477.json         new file:   writable/debugbar/debugbar_1736513972.708437.json         renamed:    writable/debugbar/debugbar_1736506951.033828.json -> writabl e/debugbar/debugbar_1736513989.114161.json         renamed:    writable/debugbar/debugbar_1736506930.003837.json -> writabl e/debugbar/debugbar_1736513993.476375.json         renamed:    writable/debugbar/debugbar_1736506918.798530.json -> writabl e/debugbar/debugbar_1736514008.758587.json         new file:   writable/debugbar/debugbar_1736514009.130154.json         new file:   writable/debugbar/debugbar_1736514075.793143.json         modified:   writable/logs/log-2025-01-10.log         new file:   writable/session/ci_session44h72p7cj7jfnkur97inmcd94ehrm1h6         new file:   writable/session/ci_session4u3huvrnofj5teuhvaogipddp0j1aj4o         new file:   writable/session/ci_session7v65rcg28k0gkqjn0petcdr68u31q2oa         new file:   writable/session/ci_session9cbgl4nsqlevtqg335j8v3d6pvn4uduh         new file:   writable/session/ci_sessionb7ad32u52f9brdr999p7tqqe452o7ht4         new file:   writable/session/ci_sessionbhdk3lsebp8sanmoquqtd69s7pna75nv         new file:   writable/session/ci_sessionbifrathmictdvlpmd85207607qs3do4t         new file:   writable/session/ci_sessionbs4v8t3g1a0chcs0nf9n174npr7s1161         new file:   writable/session/ci_sessiondtihoju17vjt323apfs5veb2clo3qhqd         new file:   writable/session/ci_sessionfcjmhp2nk22g54va0u0kmgtigke3dts2         new file:   writable/session/ci_sessionhep9admgbjjn2m3mittffdnnte7pap96         new file:   writable/session/ci_sessionm69ifmi5r2tu8shrl4v6g2scqcj7h9kn         modified:   writable/session/ci_sessionvcc2r04ordsld87kt0ecodcm3mrblhk8

--- app/Views/components/pesanan_pengguna.php
+++ app/Views/components/pesanan_pengguna.php
@@ -14,9 +14,10 @@
 </head>
 <body>
     <div class="container">
-        <h2>Tambah Pesanan</h2>
+        <div class="header">
+            <h2>Tambah Pesanan</h2>
+        </div>
         <hr>
-
         <div class="form-group">
             <form action="<?= base_url('/pesanan/tambah-pesanan-pengguna'); ?>" method="post">
                 <?= csrf_field(); ?>
@@ -34,7 +35,7 @@
 
                 <div class="form-pesanan">
                     <div class="mb-3">
-                        <label for="jenisPesanan" class="form-label">Jenis Pesanan</label>
+                        <label for="jenisPesanan" class="form-label">Jenis:</label>
                         <select id="jenisPesanan" name="jenis_pesanan" class="input form-control" required>
                             <option value="">Pilih Jenis Pesanan</option>
                             <option value="Servis Laptop">Servis Laptop</option>
@@ -45,19 +46,21 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="merekPesanan" class="form-label">Merek Pesanan</label>
+                        <label for="merekPesanan" class="form-label">Merek:</label>
                         <select id="merekPesanan" name="merek_pesanan" class="input form-control" required>
                             <option value="">Pilih Merek</option>
                         </select>
                     </div>
 
                     <div class="mb-3">
-                        <label for="kategoriPesanan" class="form-label">Kategori Pesanan</label>
+                        <label for="kategoriPesanan" class="form-label">Kategori:</label>
                         <select id="kategoriPesanan" name="kategori_pesanan" class="input form-control" required>
                             <option value="">Pilih Kategori</option>
                         </select>
                     </div>
+                </div>
 
+                <div class="form-ketentuan">
                     <div class="mb-3">
                         <label for="ketentuan_servis" class="form-label">Ketentuan Servis:</label>
                         <select name="ketentuan_servis" class="input form-control" id="ketentuan_servis">
@@ -67,24 +70,21 @@
                         </select>
                     </div>
 
-                </div>
-
-                <div class="form-ketentuan">
                     <div class="mb-3">
-                        <label for="tanggal_pesanan" class="form-label">Tanggal Pesanan</label>
+                        <label for="tanggal_pesanan" class="form-label">Tanggal:</label>
                         <input type="date" class="form-control" name="tanggal_pesanan" id="tanggal_pesanan" value="<?= set_value('tanggal_pesanan') ?>" required>
                     </div>
 
                     <div class="mb-3">
-                        <label for="jumlahPesanan" class="form-label">Jumlah Pesanan</label>
-                        <input type="number" id="jumlahPesanan" name="jumlah_pesanan" class="input form-control mt-1" min="1" required>
+                        <label for="jumlahPesanan" class="form-label">Jumlah:</label>
+                        <input type="number" id="jumlahPesanan" name="jumlah_pesanan" class="input form-control" min="1" required>
                     </div>
                 </div>
 
                 <div class="form-deskripsi">
                     <div class="mb-3">
-                        <label for="deskripsiPesanan" class="form-label">Deskripsi Pesanan</label>
-                        <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="input form-control mt-1" rows="3" required></textarea>
+                        <label for="deskripsiPesanan" class="form-label">Deskripsi:</label>
+                        <textarea id="deskripsiPesanan" name="deskripsi_pesanan" class="input form-control" rows="3" required></textarea>
                     </div>
                 </div>
 
